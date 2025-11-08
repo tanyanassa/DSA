16a8c0ae70e9f5685256574615ba2428d9dcfe15
Tanya Nassa <tanyanassa07@gmail.com>
2025-11-08 13:37:03 +0530

    duplicates remove

---
+++ linkedlistt/removeduplicates1.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+struct node
+{
+int data;
+struct node*next;
+node(int data1,struct node*next1)
+{
+data=data1;
+next=next1;
+}
+node(int data1)
+{
+data=data1;
+next=nullptr;
+}
+};
+struct node*createlist(vector<int>&array)
+{
+struct node*head=new node(array[0]);
+struct node*mover=head;
+for(int i=1;i<array.size();i++)
+{
+struct node*newnode=new node(array[i]);
+mover->next=newnode;
+mover=mover->next;
+}
+return head;
+}
+void print(struct node*head)
+{
+while(head!=NULL)
+{
+cout<<" "<<head->data<<" ";
+head=head->next;
+}
+}
+struct node* removeduplicates(struct node*head)
+{
+struct node*temp=head;
+struct node*mover;
+mover=temp->next;
+while(mover!=NULL)
+{
+while(mover->data==temp->data)
+{
+mover=mover->next;
+}
+temp->next=mover;
+temp=temp->next;
+mover=mover->next;
+}
+return head;
+}
+int main()
+{
+vector<int>array={1,2,3,4,5,5,5,5};
+struct node*head=createlist(array);
+print(head);
+cout<<endl;
+head=removeduplicates(head);
+print(head);
+cout<<endl;
+return 0;
+}
